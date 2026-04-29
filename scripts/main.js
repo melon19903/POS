8ed01f72fe447299fe7d0ad6abf6b33e0cc2423a
xia <melon2281337@gmail.com>
2026-04-29 10:21:52 +0300

    Добавлена галерея

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,9 @@ if (contactForm) {
         this.reset();
     });
 }
+let images = ['10', '20', '30', '40'];
+let currentIdx = 0;
+function nextImg() {
+    currentIdx = (currentIdx + 1) % images.length;
+    document.getElementById('galleryImage').src = 'https://picsum.photos/id/' + images[currentIdx] + '/600/400';
+}
